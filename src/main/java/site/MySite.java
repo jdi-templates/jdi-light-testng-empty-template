ebf00f22c7094b1a3c79e9ec5f3ffd4c21314ec6
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2022-07-28 14:13:10 -0500

    Urls fixes and pages mentions

--- src/main/java/site/MySite.java
+++ src/main/java/site/MySite.java
@@ -4,7 +4,8 @@ import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 import site.pages.*;
 
 public class MySite {
-    @Url("{HOME_PAGE_URL}")
+    //@Url("{ENTER_HOME_PAGE_URL}")
     public static FirstPage homePage;
 
+    // ADD SITE PAGES WITH URLS
 }
