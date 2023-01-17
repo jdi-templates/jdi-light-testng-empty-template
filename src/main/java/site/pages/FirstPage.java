11e82f0f7ea244ae2fd9465159fb8b9dabe6e723
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-16 21:47:51 -0500

    Update to 1.4.7. Comments are added for example descriptions

--- src/main/java/site/pages/FirstPage.java
+++ src/main/java/site/pages/FirstPage.java
@@ -4,6 +4,7 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
+// This is an example of Page Object definition. To be removed
 public class FirstPage extends WebPage {
     @UI("//button")
     public static Button testButton;
