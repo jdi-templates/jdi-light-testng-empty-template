632a6f0275fe10f6b4d17fb6d2bc9c31a5a1fdd0
vklonin <vklonin@gmail.com>
2023-07-31 15:33:58 +0300

    Added unused import for convenience

--- src/main/java/site/pages/FirstPage.java
+++ src/main/java/site/pages/FirstPage.java
@@ -3,6 +3,7 @@ package site.pages;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
+import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
 
 // This is an example of Page Object definition. To be removed
 public class FirstPage extends WebPage {
